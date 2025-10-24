8d81734c1d31cb790b07a0bfd32c26fbb7d31e0b
SofiaaaRA <Sofia.Romero.Angel@gmail.com>
2025-10-23 19:42:51 -0700

    actrive list

---
+++ hello world/src/DoublyLinkedList.java
@@ -0,0 +1,49 @@
+package org.example.sandbox.linkedlist;
+
+public class DoublyLinkedList<E> implements LinkedList<E> {
+
+    @Override
+    public void addFirst(E element) {
+
+    }
+
+    @Override
+    public void addLast(E element) {
+
+    }
+
+    @Override
+    public E pollFirst() {
+        return null;
+    }
+
+    @Override
+    public E pollLast() {
+        return null;
+    }
+
+    @Override
+    public E peekFirst() {
+        return null;
+    }
+
+    @Override
+    public E peekLast() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
